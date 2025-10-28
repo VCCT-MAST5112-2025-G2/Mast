0cf382a2da723d60d0bc019e2f87efd68cb1a231
Wayden September <ST10489566@vcconnect.edu.za>
2025-10-28 14:14:07 +0200

    test commit

--- src/screens/MenuItemCard.tsx
+++ src/screens/MenuItemCard.tsx
@@ -17,7 +17,7 @@ const MenuItemCard: React.FC<CardProps> = ({ item }) => {
   );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ fsjgfyi
   card: {
     width: 300,
     borderWidth: 1,
